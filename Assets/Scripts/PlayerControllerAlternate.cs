61fc679f264c09a09bcb6db28ed6ad0c401406c4
PastaTime <arran94@gmail.com>
2022-07-24 14:21:28 +1200

    Advanced haptics, heart pacemaker implemented, alternate player character. (#11)
    Co-authored-by: John Bu <chocolatehotfries@gmail.com>

---
+++ Assets/Scripts/PlayerControllerAlternate.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class PlayerControllerAlternate : MonoBehaviour
+{
+    public float speed = 100f; // Speed variable
+    public float turnSpeed = 1.0f; // Turn speed variable
+    public Rigidbody rb; // Set the variable 'rb' as Rigibody
+    public Vector3 movement; // Set the variable 'movement' as a Vector3 (x,y,z)
+    public bool alwaysFaceNorth = true;
+
+    float rotateInput = 0.0f;
+    Vector3 autoRotation;
+ 
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+ 
+
+    void Update()
+    {
+        rotateInput = Input.GetAxis("RotateX");
+        Debug.Log(rotateInput);
+        movement = (transform.forward * Input.GetAxis("Vertical")) + (transform.right * Input.GetAxis("Horizontal"));
+    }
+ 
+ 
+    void FixedUpdate()
+    {
+        moveCharacter(movement); // We call the function 'moveCharacter' in FixedUpdate for Physics movement
+    }
+ 
+ 
+    void moveCharacter(Vector3 direction)
+    {
+        if (alwaysFaceNorth)
+        {
+            rb.velocity = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical")) * speed * Time.fixedDeltaTime;
+        }
+        else
+        {
+            rb.velocity = direction * speed * Time.fixedDeltaTime;
+            transform.Rotate(Vector3.up * rotateInput * turnSpeed * Time.fixedDeltaTime);
+        }
+    }
+ 
+}
